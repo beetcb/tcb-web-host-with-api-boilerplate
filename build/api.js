3284e6d81e38e5b335d321944e71f4d7d8c9cd08
beet <zpharno@163.com>
2021-12-24 22:01:37 +0800

    perf: faster build algo

--- build/api.js
+++ build/api.js
@@ -1,17 +1,21 @@
 const nextDefaultApiPath = "pages/api/";
 require("fs").readdir(nextDefaultApiPath, (err, files) => {
-  files.forEach((file) => {
-    require("esbuild").buildSync({
-      entryPoints: [`${nextDefaultApiPath}${file}`],
-      entryNames: "[dir]/[name]/index",
-      outbase: "pages",
-      // Why we bundle?
-      // - We might want to import external modules outside `api` dir, bundle it is necessary
-      // - Bundle dependencies so we don't have to add `package.json` file
-      bundle: true,
-      platform: "node",
-      target: ["node10.15"],
-      outdir: "out",
-    });
-  });
+  Promise.all(
+    files.map((file) => {
+      require("esbuild")
+        .build({
+          entryPoints: [`${nextDefaultApiPath}${file}`],
+          entryNames: "[dir]/[name]/index",
+          outbase: "pages",
+          // Why we bundle?
+          // - We might want to import external modules outside `api` dir, bundle it is necessary
+          // - Bundle dependencies so we don't have to add `package.json` file
+          bundle: true,
+          platform: "node",
+          target: ["node10.15"],
+          outdir: "out",
+        })
+        .then(console.log);
+    })
+  );
 });
